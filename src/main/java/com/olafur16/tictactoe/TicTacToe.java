368fd27d3bf7f276650648a3e679220d0dbb2f96
olafurt15 <olafurt15@ru.is>
2016-10-25 11:57:24 +0000

    Er Búinn að Initialisea game board og gera tests
    Tvívítt array notað fyrir gameboard

--- src/main/java/com/olafur16/tictactoe/TicTacToe.java
+++ src/main/java/com/olafur16/tictactoe/TicTacToe.java
@@ -2,6 +2,31 @@ package com.olafur16.tictactoe;
 
 public class TicTacToe
 {
+	public static final int boardSize = 3;
+	private char[][] board = new char[boardSize][boardSize];
+
+	public TicTacToe()
+	{
+		fillBoard();
+	}
+	public void fillBoard()
+	{
+		char nextNumberToFill = '1';
+
+    	for(int j = 0; j < boardSize; j++)
+    	{
+       		for(int i = 0; i < boardSize; i++)
+        	{
+            	board[j][i] = nextNumberToFill;
+            	nextNumberToFill++;
+        	}
+    	}
+
+	}
+	public char charAt(int posX, int posY)
+	{
+		return board[posX][posY];
+	}
 	public static void main(String[] args)
 	{
 
